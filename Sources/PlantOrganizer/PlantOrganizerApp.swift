62beb57e95cc8fc455c7161fb2242791f6b5b224
Kiryl2217 <ramemkelvin@gmail.com>
2026-09-16 14:05:49 +0300

    Create PlantOrganizerApp.swift

---
+++ Sources/PlantOrganizer/PlantOrganizerApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+public struct PlantOrganizerApp: App {
+    public init() {}
+    
+    public var body: some Scene {
+        WindowGroup {
+            MainPlantListView()
+        }
+    }
+}
